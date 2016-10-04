dd8a2a86ef1ae7ad6a818438b6e2804bb872606a
James <jumpinjimmy@gmail.com>
2016-10-03 19:16:34 -0600

    exercis 7.1 finished

--- epi_ch7_strings/chap_seven.cc
+++ epi_ch7_strings/chap_seven.cc
@@ -3,11 +3,36 @@ StringExercises::StringExercises() {}
 
 StringExercises::~StringExercises() {}
 
-std::string StringExercises::IntToString(int x) {
-    std::string result("hello");
+std::string StringExercises::IntToString(int in_int) {
+    std::string result;
+    bool negative = false;
+    if (in_int < 0) {
+        negative = true;
+        in_int = -in_int; /// negate so @in_int is positive
+    }
+
+    do {
+        result += '0' + in_int % 10;
+        in_int /= 10;
+    } while (in_int);
+
+    if (negative) {
+        result += '-';
+    }
+
+    std::reverse(result.begin(), result.end());
     return result;
 }
 
 int StringExercises::StringToInt(std::string &in_str) {
-    return 0;
+    int result = 0;
+    bool negative = in_str[0] == '-';
+    int i = negative ? 1 : 0;
+
+    for (; i < in_str.size(); ++i) {
+        int digit = in_str[i] - '0';
+        result = result * 10 + digit;
+    }
+
+    return negative ? -result : result;
 }
--- epi_ch7_strings/main.cc
+++ epi_ch7_strings/main.cc
@@ -5,15 +5,18 @@ using namespace std; //NOLINT
 
 void InterConversion(StringExercises* string_exerciser) {
     std::cout << "--->>--->> main::InterCoversion "  << std::endl;
-    int starting_integer = 14;
+    int starting_integer = -14;
     std::string res_str = string_exerciser->IntToString(starting_integer);
+    std::cout << "IntToString(" << starting_integer << ") result: " << res_str << std::endl;
     int res_int = string_exerciser->StringToInt(res_str);
+    std::cout << "StringToInt(" << res_str << ") result: " << res_int << std::endl;
 }
 
 void run_tests(StringExercises* string_exerciser) {
     InterConversion(string_exerciser);
 }
 
+// valgrind --leak-check=full --show-leak-kinds=all ./ch7_test
 int main(int argc, char const *argv[]) {
     std::shared_ptr<StringExercises> string_exerciser(new StringExercises());
     run_tests(string_exerciser.get());
