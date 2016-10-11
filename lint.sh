9a1a90d96abbc01b34148bff364f95dfff3bd8a1
James <jumpinjimmy@gmail.com>
2016-10-11 16:07:36 -0600

    add updated cpplint python script

--- lint.sh
+++ lint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 echo "Linting source files"
-find ./ \( -name "*.h" -o -name "*.cc" \) -type f | grep -v pb | xargs python cpplint.py --root=Algs-and-DataStructure-Implementations/ --header-guard-prefix="" --filter=-legal/copyright,-build/include,-whitespace/comments,-readability/streams,-runtime/references,-readability/casting,-runtime/arrays,-runtime/printf
+# find ./ \( -name "*.h" -o -name "*.cc" \) -type f | grep -v pb | xargs python cpplint.py --root=Algs-and-DataStructure-Implementations/ --header-guard-prefix=""  --filter=-legal/copyright,-build/include,-whitespace/comments,-readability/streams,-runtime/references,-readability/casting,-runtime/arrays,-runtime/printf
+
+# find ./ \( -name "*.h" -o -name "*.cc" \) -type f | grep -v pb | xargs python cpplint.py --root=Algs-and-DataStructure-Implementations/  --linelength=150 --extensions=cc,h --filter=-legal/copyright,-build/include,-whitespace/comments,-readability/streams,-runtime/references,-readability/casting,-runtime/arrays,-runtime/printf ./*/*
+
+find ./ \( -name "*.h" -o -name "*.cc" \) -type f | grep -v pb | xargs python cpplint.py --linelength=150 --extensions=cc,h --filter=-legal/copyright,-build/include,-whitespace/comments,-readability/streams,-runtime/references,-readability/casting,-runtime/arrays,-runtime/printf ./*/*
