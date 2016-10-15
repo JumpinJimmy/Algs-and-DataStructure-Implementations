e62039539c44ed29b46e682381e3973d193a5591
James <jumpinjimmy@gmail.com>
2016-10-15 17:03:49 -0600

    add binary tree node interface, remove unneeded linked list prototype classes from chapter 10 exercises

---
+++ epi_ch10_binary-trees/includes/binary_tree_node.h
@@ -0,0 +1,12 @@
+#ifndef BINARY_TREE_NODE_H
+#define BINARY_TREE_NODE_H
+#include <memory>
+using std::unique_ptr;
+
+template <typename T>
+struct BinaryTreeNode {
+  T data;
+  unique_ptr<BinaryTreeNode<T>> left, right;
+};
+
+#endif // BINARY_TREE_NODE_H
