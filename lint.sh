b21e6537004c74e234f5d60cb2d50309b711bfbd
James DeVore <james.r.devore@seagate.com>
2016-10-17 15:26:50 -0600

    update cpplint script and lint.sh call. still need to work on which filters to exclude

--- lint.sh
+++ lint.sh
@@ -5,4 +5,20 @@ echo "Linting source files"
 
 # find ./ \( -name "*.h" -o -name "*.cc" \) -type f | grep -v pb | xargs python cpplint.py --root=Algs-and-DataStructure-Implementations/  --linelength=150 --extensions=cc,h --filter=-legal/copyright,-build/include,-whitespace/comments,-readability/streams,-runtime/references,-readability/casting,-runtime/arrays,-runtime/printf ./*/*
 
-find ./ \( -name "*.h" -o -name "*.cc" \) -type f | grep -v pb | xargs python cpplint.py --linelength=150 --extensions=cc,h --filter=-legal/copyright,-build/include,-whitespace/comments,-readability/streams,-runtime/references,-readability/casting,-runtime/arrays,-runtime/printf ./*/*
+find ./ \( -name "*.h" -o -name "*.cc" \) -type f | grep -v pb | xargs python cpplint.py --verbose=1 --linelength=150 --root=Algs-and-DataStructure-Implementations/ --extensions=h --headers=h --filter=-legal/copyright,-readability/check,+whitespace/todo ./*/*
+
+# python cpplint.py
+# --verbose=1
+# --exclude_files=.*\.png, .*\.md
+# --root=Algs-and-DataStructure-Implementations/
+# --linelength=150
+# --extensions=cc,h
+# --headers=h
+# --filter=-build/include,-whitespace/comments,-runtime/references,-readability/casting,-runtime/arrays,-runtime/printf ./*/*
+# whitespace/todo
+
+# python cpplint.py --verbose=1 --linelength=150 --root=Algs-and-DataStructure-Implementations/ --extensions=cc,h --headers=h --filter=-legal/copyright,-build/include,-whitespace/comments,-runtime/references,-readability/casting,-runtime/arrays,-runtime/printf ./*/*
+
+
+# python cpplint.py --verbose=1 --linelength=150 --root=Algs-and-DataStructure-Implementations/ --extensions=cc,h --headers=h --filter=-whitespace/todo ./*/*
+# python cpplint.py --verbose=1 --linelength=150 --root=Algs-and-DataStructure-Implementations/ --extensions=cc,h --headers=h --filter=-legal/copyright,-readability/check,+whitespace/todo ./*/*
