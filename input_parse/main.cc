b196fc0f0567775e0e6ab2bbd6b2231ac8919d39
James DeVore <james.r.devore@seagate.com>
2017-01-12 13:35:08 -0700

    basic function map in place

--- input_parse/main.cc
+++ input_parse/main.cc
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <map>
 #include <sstream>
+#include <functional>
+
 #include <memory>
 #include <set>
 
@@ -35,10 +37,36 @@ bool TestSuiteThree(std::unique_ptr<DummyClass> &dummy_class, const std::set<std
     return true;
 }
 
+bool functionptr_test(int a, std::string x) {
+    std::cout << "======= main::bool functionptr_test(a: " << a << ", string: " << x << ") CALLED! " << std::endl;
+    return true;
+}
+
+bool functionptr_test_two(int a, std::string x) {
+    std::cout << "======= main::bool functionptr_test_two(a: " << a << ", string: " << x << ") CALLED! " << std::endl;
+    return true;
+}
+
 // valgrind --leak-check=full --show-leak-kinds=all ./input_parse
 int main(int argc, char const *argv[]) {
+    // std::map<std::string, bool (std::unique_ptr<DummyClass> &, const std::set<std::string> &)> function_map_;
+    std::map<std::string, std::function<bool(int, std::string)>> function_map_;
+
+    // std::function<bool(int)> f_display = functionptr_test;
+    function_map_["functionptr_test"] = functionptr_test;
+    function_map_["functionptr_test_two"] = functionptr_test_two;
     std::unique_ptr<DummyClass> dummy_class(new DummyClass());
     std::set<std::string> device_blacklist {"A"};
+    int device_path = std::stoi(argv[1]);
+
+    if (device_path == 1) {
+        function_map_["functionptr_test"] (1, "call one");
+    } else if (device_path == 2) {
+        function_map_["functionptr_test_two"] (2, "call two");
+    } else {
+        std::cout << "======= nuffin"<< std::endl;
+    }
+    // f_display(12);
 
     /* code */
     return 0;
