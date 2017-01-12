2f7d92217cee841e200b79e092f516a726d3f596
James DeVore <james.r.devore@seagate.com>
2017-01-12 13:38:41 -0700

    calls via function map with unique pointer and set

--- input_parse/main.cc
+++ input_parse/main.cc
@@ -50,19 +50,29 @@ bool functionptr_test_two(int a, std::string x) {
 // valgrind --leak-check=full --show-leak-kinds=all ./input_parse
 int main(int argc, char const *argv[]) {
     // std::map<std::string, bool (std::unique_ptr<DummyClass> &, const std::set<std::string> &)> function_map_;
-    std::map<std::string, std::function<bool(int, std::string)>> function_map_;
+    // std::map<std::string, std::function<bool(int, std::string)>> function_map_;
+    std::map<std::string, std::function<bool(std::unique_ptr<DummyClass> &, const std::set<std::string> &)>> function_map_;
+    function_map_["TestSuiteOne"] = TestSuiteOne;
+    function_map_["TestSuiteTwo"] = TestSuiteTwo;
 
     // std::function<bool(int)> f_display = functionptr_test;
-    function_map_["functionptr_test"] = functionptr_test;
-    function_map_["functionptr_test_two"] = functionptr_test_two;
+    // function_map_["functionptr_test"] = functionptr_test;
+    // function_map_["functionptr_test_two"] = functionptr_test_two;
     std::unique_ptr<DummyClass> dummy_class(new DummyClass());
     std::set<std::string> device_blacklist {"A"};
     int device_path = std::stoi(argv[1]);
 
+    // if (device_path == 1) {
+    //     function_map_["functionptr_test"] (1, "call one");
+    // } else if (device_path == 2) {
+    //     function_map_["functionptr_test_two"] (2, "call two");
+    // } else {
+    //     std::cout << "======= nuffin"<< std::endl;
+    // }
     if (device_path == 1) {
-        function_map_["functionptr_test"] (1, "call one");
+        function_map_["TestSuiteOne"] (dummy_class, device_blacklist);
     } else if (device_path == 2) {
-        function_map_["functionptr_test_two"] (2, "call two");
+        function_map_["TestSuiteTwo"] (dummy_class, device_blacklist);
     } else {
         std::cout << "======= nuffin"<< std::endl;
     }
