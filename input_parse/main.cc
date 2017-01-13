68857038123906427a9e4f3408207fdd0a5e0a42
James DeVore <james.r.devore@seagate.com>
2017-01-13 12:46:12 -0700

    bookmark before clean up and new approach to parse

--- input_parse/main.cc
+++ input_parse/main.cc
@@ -53,9 +53,7 @@ bool functionptr_test_two(int a, std::string x) {
 }
 
 void usage(){
-    std::cout<<"Usage:: \"./FileMetrics <rootDirectoryName> <#directories> <#files> <SizeOfFiles> <ReadPercentage>\" \n";
-    std::cout<<"Usage:: \"SizeOfFile: <0,4,32,64,128,256,512,1024> (0kb -> 1MB)\" \n";
-    std::cout<<"Usage:: \"ReadPercentage: Percentage of Directories you want to Read (i.e. 30 == Read 30\% of the directories)\" \n"<<std::endl;
+    std::cout<<"Usage:: \"./input_parse <dev> <test> \n";
 }
 // /////////////////////////
 // args
@@ -69,6 +67,7 @@ void usage(){
 // http://courses.cms.caltech.edu/cs11/material/c/mike/misc/cmdline_args.html
 // http://stackoverflow.com/questions/5272550/c-command-line-parameters
 // http://stackoverflow.com/questions/15344714/convert-command-line-argument-to-string
+// http://www.cplusplus.com/forum/articles/13355/
 int main(int argc, char const *argv[]) {
     std::map<std::string, std::function<bool(std::unique_ptr<DummyClass> &, const std::set<std::string> &)>> function_map_;
     function_map_["A"] = TestSuiteOne;
@@ -78,17 +77,67 @@ int main(int argc, char const *argv[]) {
     std::unique_ptr<DummyClass> dummy_class(new DummyClass());
     std::set<std::string> device_blacklist {};
     std::string target_device = "";
+
+    if (argc < 2) {
+        std::cout<<"INCORRECT AMOUNT OF ARGUMENTS"<<std::endl;
+        usage();
+        exit(EXIT_FAILURE);
+    } else {
+        std::vector<std::string> args;
+        std::copy(argv + 1, argv + argc, std::back_inserter(args));
+        int param_count = argc;
+        std::cout << "======= main:: arg count " << param_count << std::endl;
+        unsigned int param_flag_index = 0;
+        unsigned int param_val_index = 1;
+
+        // while ()
+        // for (auto str : args) {
+        //     std::cout << "======= main:: arg = " << str << std::endl;
+        //     if (str.compare("-d")) {
+
+        //     }
+        // }
+    // for (auto iter = args.begin(); iter != args.end(); ++iter) {
+    //     std::cout << *iter << ' ';
+    //     if (*iter.compare("-d")) {
+    //         target_device = *std::next(iter,1);
+    //         std::cout << "======= main:: target_device " << target_device << std::endl;
+    //     }
+    }
+        // for (int i = 1; i < param_count; ++i) {
+        //     if (strcmp(argv[i], "-d") == 0) {
+        //         if (argv[i][0] == "")
+        //     }
+        //     // if (i + 1 != param_count) {
+        //         std::cout << "======= main::argv[" << i << "] = " << argv[i] << std::endl;
+        //     // }
+        // }
+    // }
+        // if ( strcmp(argv[i], "-v") == 0 ) {
+
+        //     zbc_set_log_level("debug");
+
+        // } else if ( strcmp(argv[i], "-n") == 0 ) {
+
+        //     num = 1;
+
+        //  } else if ( strcmp(argv[i], "-nz") == 0 ) {
+
+        //     if ( i >= (argc - 1) ) {
+        //         goto usage;
+        //     }
+        //     i++;
     // std::vector<std::string> device_list {};
 
-    int device_path = std::stoi(argv[1]);
+    // int device_path = std::stoi(argv[1]);
 
-    if (device_path == 1) {
-        function_map_["A"] (dummy_class, device_blacklist);
-    } else if (device_path == 2) {
-        function_map_["B"] (dummy_class, device_blacklist);
-    } else {
-        std::cout << "======= nuffin"<< std::endl;
-    }
+    // if (device_path == 1) {
+    //     function_map_["A"] (dummy_class, device_blacklist);
+    // } else if (device_path == 2) {
+    //     function_map_["B"] (dummy_class, device_blacklist);
+    // } else {
+    //     std::cout << "======= nuffin"<< std::endl;
+    // }
 
     return 0;
 }
