bc988628548f45803c900f219cc72cbf91415e2f
James <jumpinjimmy@gmail.com>
2017-01-18 20:33:54 -0700

    finsihed 14.1 exercise. No Leaks, no lint error

--- epi_ch14_sorting/chap_fourteen.cc
+++ epi_ch14_sorting/chap_fourteen.cc
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <algorithm>
 #include "includes/chap_fourteen.h"
 
 SortingExercises::SortingExercises() {}
@@ -6,5 +6,15 @@ SortingExercises::SortingExercises() {}
 SortingExercises::~SortingExercises() {}
 
 std::vector<int> SortingExercises::ComputeListIntersection(const std::vector<int>& A1, const std::vector<int>& A2) {
-    return {};
+    PrintCollection(A1, "ComputeIntersection List1: ");
+    PrintCollection(A2, "ComputeIntersection List2: ");
+    std::vector<int> result = {};
+    int index = 0;
+    for (auto &val : A1) {
+        if (((index == 0) || (val != A1.at(index-1))) && (std::binary_search(A2.begin(), A2.end(), val))) {
+            result.emplace_back(val);
+        }
+        ++index;
+    }
+    return result;
 }
--- epi_ch14_sorting/includes/chap_fourteen.h
+++ epi_ch14_sorting/includes/chap_fourteen.h
@@ -1,12 +1,22 @@
 #ifndef CHAP_FOURTEEN_H
 #define CHAP_FOURTEEN_H
 #include <vector>
+#include <string>
 
 class SortingExercises {
  public:
     SortingExercises();
     ~SortingExercises();
     std::vector<int> ComputeListIntersection(const std::vector<int>& A1, const std::vector<int>& A2);
+
+    template <typename T>
+    void PrintCollection(const std::vector<T> &v, const std::string &extra_detail = "list: ") {
+        std::cout << extra_detail << " size: " << std::dec << v.size() << "\n [ ";
+        for (auto &val : v) {
+            std::cout << val << " ";
+        }
+        std::cout << "];" << std::endl;
+    }
 };
 
 #endif  // CHAP_FOURTEEN_H
