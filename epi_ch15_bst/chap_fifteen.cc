2198f9a218a7d2800167bb6e2f6c1258786d52b2
James <jumpinjimmy@gmail.com>
2017-01-22 14:57:54 -0700

    pseudocode for 15.1 alternate, 15.2 and 15.3

--- epi_ch15_bst/chap_fifteen.cc
+++ epi_ch15_bst/chap_fifteen.cc
@@ -12,6 +12,51 @@ bool BstExercises::IsBST(const std::unique_ptr<BSTNode<int>> &tree) {
     return CheckKeysIsBST(tree, std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
 }
 
+bool BstExercises::IsBinarySearchTreeDFS(const std::unique_ptr<BSTNode<int>> &tree) {
+    // PSEUDO
+    /*
+    Depth first Search Tree, storing upper and lower bounds alongside each node in traversal queue
+    struct BSTValue { key, lower, upper}
+    queue<BSTValue> q.enqueue(tree, numeric_limits:min, numeric_limits:max)
+
+    while (!q.empty()) {
+        if (q.front().tree_node.get()) {
+            if (q.front().data < q.front.data.min || q.front().data > q.front.data.max) return false
+            q.enqueue(q.front().tree_node.left(), q.front().lower, q.front().tree_node.data)
+            q.enqueue(q.front().tree_node.right(),q.front().tree_node.data, q.front().upper )
+        }
+        q.pop()
+    }
+    return true;
+     */
+
+    return true;
+}
+
+BSTNode<int>* BstExercises::FirstGreaterKey(const std::unique_ptr<BSTNode<int>> &tree, int key) {
+    // PSEUDO: Search BST using bst property (binary search) and keep track of most recently visited node
+    /*
+        subtree_node = tree; result = nullptr
+        while (subtree_node)
+            if (subtree_node.data > Key) {
+                result = subtree_node
+                subtree_node = subtree_node.left
+            } else {
+                subtree_node = subtree_node.right
+            }
+        return result
+     */
+    BSTNode<int> *result = tree.get();
+    return result;
+}
+
+std::vector<int> RetreiveKLargestKeys(const std::unique_ptr<BSTNode<int>> &tree, int k) {
+    // PSEUDO: K Largest Keys, Visit / traverse the tree from largest to smallest until we have K entries
+    // right subtree, root / node , left subtree  == largest to smallest
+    // store results in a list as we traverse backwards. Stop when list is of size K
+    return {};
+}
+
 bool BstExercises::CheckKeysIsBST(const std::unique_ptr<BSTNode<int>> &bst_node, int min, int max) {
     if (!bst_node) return true;
     if (bst_node->data > max || bst_node->data < min) {
--- epi_ch15_bst/includes/chap_fifteen.h
+++ epi_ch15_bst/includes/chap_fifteen.h
@@ -1,6 +1,7 @@
 #ifndef CHAP_FIFTEEN_H
 #define CHAP_FIFTEEN_H
 #include <memory>
+#include <vector>
 #include "./bst_prototype.h"
 
 class BstExercises {
@@ -8,6 +9,9 @@ class BstExercises {
     BstExercises();
     ~BstExercises();
     bool IsBST(const std::unique_ptr<BSTNode<int>> &tree);
+    bool IsBinarySearchTreeDFS(const std::unique_ptr<BSTNode<int>> &tree);
+    BSTNode<int>* FirstGreaterKey(const std::unique_ptr<BSTNode<int>> &tree, int key);
+    std::vector<int> RetreiveKLargestKeys(const std::unique_ptr<BSTNode<int>> &tree, int k);
 
  private:
     bool CheckKeysIsBST(const std::unique_ptr<BSTNode<int>> &bst_node, int min, int max);
